373f4caaf36ddb44875faa216a9d1d07192c2561
Dhruv <dhruv2295@gmail.com>
2022-02-03 15:17:18 +0530

    update: root db added for offline catching

--- app/src/main/java/com/nasa/image/MainActivity.kt
+++ app/src/main/java/com/nasa/image/MainActivity.kt
@@ -5,32 +5,40 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nasa.image.ActivityFunctionManager.MainManager
 import com.nasa.image.adapter.NasaListAdapter
 import com.nasa.image.database.Nasa
+import com.nasa.image.database.NasaDatabase
 import com.nasa.image.databinding.ActivityMainBinding
 import com.nasa.image.network.NetworkManager
+import com.nasa.image.repo.NasaRepository
 import com.nasa.image.viewModels.MainViewModel
+import com.nasa.image.viewModels.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
     private lateinit var nasaListAdapter: NasaListAdapter
     private lateinit var networkManager: NetworkManager
     private lateinit var mainManager: MainManager
     private var nasaList = mutableListOf<Nasa>()
+    private var isAppFirstStart = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val dao = NasaDatabase.getDatabase(applicationContext).nasaDao()
+        val repository = NasaRepository(dao)
+
         mainManager = MainManager(this)
         nasaListAdapter = NasaListAdapter(nasaList)
-
-        binding.sDate.text = mainManager.getLastMonth()
-        binding.eDate.text = mainManager.getToday()
+        mainViewModel =
+            ViewModelProvider(this, MainViewModelFactory(repository))[MainViewModel::class.java]
 
         networkManager = NetworkManager(this, object: NetworkManager.IAPICallback{
             override fun onSuccess(isSuccess: Boolean, list: List<Nasa>) {
@@ -38,21 +46,35 @@ class MainActivity : AppCompatActivity() {
                 binding.request.visibility = View.GONE
                 binding.refresh.isRefreshing = false
                 binding.request.isClickable = true
+
                 if (isSuccess) {
-                    nasaList.clear()
-                    nasaList.addAll(list)
-                    nasaListAdapter.notifyDataSetChanged()
+                    if (isAppFirstStart){
+                        mainViewModel.clearDB()
+                        list.map { mainViewModel.insertNasa(it)}
+                    } else{
+                        nasaList.clear()
+                        nasaList.addAll(list)
+                        nasaListAdapter.notifyDataSetChanged()
+                    }
                 }
+                isAppFirstStart = false
             }
 
             override fun onFailure(errorMessage: String) {
                 binding.request.isClickable = true
                 binding.progressBar.visibility = View.GONE
                 binding.request.visibility = View.GONE
-                binding.refresh.isRefreshing = false
                 Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
             }
         })
+
+        mainViewModel.getNasa().observe(this) {
+            binding.progressBar.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
+            binding.request.visibility = View.GONE
+            nasaList.clear()
+            nasaList.addAll(it)
+            nasaListAdapter.notifyDataSetChanged()
+        }
 
         binding.nasaRecyclerView.apply {
             adapter = nasaListAdapter
@@ -60,16 +82,8 @@ class MainActivity : AppCompatActivity() {
                 LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
         }
 
-        binding.search.doOnTextChanged { text, _, _, _ ->
-
-            val l = mutableListOf<Nasa>()
-            nasaList.map {
-                if (it.title!!.lowercase().contains(text.toString().lowercase())){
-                    l.add(it)
-                }
-            }
-            nasaListAdapter.updateList(l)
-        }
+        binding.sDate.text = mainManager.getLastMonth()
+        binding.eDate.text = mainManager.getToday()
 
         binding.editEnd.setOnClickListener {
             mainManager.showDateTimePicker(
@@ -89,9 +103,20 @@ class MainActivity : AppCompatActivity() {
             binding.request.isClickable = false
             binding.progressBar.visibility = View.VISIBLE
             networkManager.getImageOfTheDay(
-                startDate = binding.sDate.text.toString(),
-                endDate = binding.eDate.text.toString()
+                startDate = binding.eDate.text.toString(),
+                endDate = binding.sDate.text.toString()
             )
+        }
+
+        binding.search.doOnTextChanged { text, _, _, _ ->
+
+            val l = mutableListOf<Nasa>()
+            nasaList.map {
+                if (it.title!!.lowercase().contains(text.toString().lowercase())){
+                    l.add(it)
+                }
+            }
+            nasaListAdapter.updateList(l)
         }
 
         binding.refresh.setOnRefreshListener {
@@ -99,6 +124,7 @@ class MainActivity : AppCompatActivity() {
                 startDate = mainManager.getLastMonth(),
                 endDate = mainManager.getToday()
             )
+            isAppFirstStart = true
         }
 
         networkManager.getImageOfTheDay(
